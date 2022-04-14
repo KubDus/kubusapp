e75daaedf3c7b7b2041935526bb02ed0ce408a49
Jakub Dus <jakub.dus@seznam.czgit config --global user.email jakub.dus@seznam.cz>
2022-04-14 20:21:39 +0200

    init

---
+++ todos.js
@@ -0,0 +1,19 @@
+//Comment for base project
+module.exports = [
+  {
+    id: 1,
+    task: "Wash dog agaaaaina"
+  },
+  {
+    id: 2,
+    task: "Buy Groceries"
+  },
+  {
+    id: 3,
+    task: "Wash the dishes"
+  },
+  {
+    id: 4,
+    task: "Make Dinner"
+  }
+];
